8bc69de0bfa0fca746032d10e4a77355d19906ed
Orbernator <github@teamorbi.net>
2026-03-19 15:01:41 +1030

    OrbiLib v0.1.2 Release
    Available on PyPI

---
+++ orbiLib/__init__.py
@@ -0,0 +1,9 @@
+from .orbiLib import __version__, greetingMessage, libUpdate, type_message, updateCheck
+
+__all__ = [
+	"__version__",
+	"updateCheck",
+	"type_message",
+	"greetingMessage",
+	"libUpdate",
+]
